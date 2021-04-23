308c9c7ad5c28961b3f0a632d3bb5adf7daf91b5
Tim O'Malley <tomalley24@gmail.com>
2021-04-23 09:34:40 -0500

    renamed server.js to index.js. moved connection to server to db folder in connection.js so it is handled outside of the hub of the app

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-//importing mysql inquirer and dotenv
+//importing mysql and dotenv
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 require('dotenv').config();
 
+//importing helper functions
+//const addEmployee = require('./src/addEmployee');
 
-//setting the options array for inquirer
-
+//creating the mysql connection using envirionment variables 
 const connection = mysql.createConnection({
     host: 'localhost',
 
     // Your port; if not 3306
     port: 3306,
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 
 });
 
@@ -47,8 +49,7 @@ const giveOptions = () => {
                 console.log('View employees by manager');
                 return giveOptions();
             case 'Add employee':
-                console.log('Add employee');
-                return giveOptions();;
+                return addEmployee();
             case 'Remove employee':
                 console.log('Remove employee');
                 return giveOptions();
@@ -61,12 +62,44 @@ const giveOptions = () => {
             case 'View all roles':
                 console.log('View all roles');
                 return giveOptions();
-            case 'exit':
+            case 'exit': //works as intended :)
                 console.log('exit');
                 return connection.end();
         }
     })
+    .catch((err) =>{
+        console.log(err)
+    });
 }
+
+const addEmployee = () =>{
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is the employees first name?',
+            name: 'firstName'
+        },
+        {
+            type: 'input',
+            message: 'What is the employees last name?',
+            name: 'lastName'
+        },
+        {
+            type: 'list',
+            name: 'choice',
+            message: 'What is the employees role',
+            choices: ['Sales Lead', 'Sales Person', 'Software Engineer', 'Engineering Manager', 'Accountant', 'Accountant Manager', 'Lawyer', 'Legal Team Lead'],
+        },
+    ])
+    .then((data) => {
+        console.log(`${data.firstName} || ${data.lastName} || ${data.choice}`);
+        giveOptions();
+    })
+    .catch((err) =>{
+        console.log(`Error: ${err}`)
+    });
+};
 
 connection.connect((err) => {
     if (err) throw err;
--- src/addEmployee.js
+++ src/addEmployee.js
@@ -1,2 +1,31 @@
-const mysql = require('mysql');
 const inquirer = require('inquirer');
+
+const addEmployee = () =>{
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is the employees first name?',
+            name: 'firstName'
+        },
+        {
+            type: 'input',
+            message: 'What is the employees last name?',
+            name: 'firstName'
+        },
+        {
+            type: 'list',
+            name: 'choice',
+            message: 'What is the employees role',
+            choices: ['Sales Lead', 'Sales Person', 'Software Engineer', 'Engineering Manager', 'Accountant', 'Accountant Manager', 'Lawyer', 'Legal Team Lead'],
+        },
+    ])
+    .then((data) => {
+        console.log(`${data.firstName} || ${data.lastName} || ${data.choice}`);
+    })
+    .catch((err) =>{
+        console.log(`Error: ${err}`)
+    });
+};
+
+module.exports = addEmployee();
